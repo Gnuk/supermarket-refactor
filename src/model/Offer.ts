b8298243030ad70fe21fab94cee7cceb3b17061f
Anthony REY <arey@ippon.fr>
2021-04-24 13:40:31 +0200

    Iteration2

--- src/model/Offer.ts
+++ src/model/Offer.ts
@@ -3,8 +3,4 @@ import { SpecialOfferType } from './SpecialOfferType';
 
 export class Offer {
   public constructor(public readonly offerType: SpecialOfferType, public readonly product: Product, public readonly argument: number) {}
-
-  getProduct(): Product {
-    return this.product;
-  }
 }
