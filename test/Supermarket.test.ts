baeccd413f46a3b13a0eb7b827c6577faa4cd6fb
Anthony REY <arey@ippon.fr>
2021-04-24 11:52:06 +0200

    Update golden master

--- test/Supermarket.test.ts
+++ test/Supermarket.test.ts
@@ -1,4 +1,3 @@
-import { Discount } from '../src/model/Discount';
 import { Product } from '../src/model/Product';
 import { ProductUnit } from '../src/model/ProductUnit';
 import { Receipt } from '../src/model/Receipt';
@@ -10,33 +9,114 @@ import { ReceiptPrinter } from '../src/ReceiptPrinter';
 
 import { FakeCatalog } from './FakeCatalog';
 
-describe('Supermarket', function () {
-  it('TODO decide what to specify', function (this: any) {
-    const catalog: SupermarketCatalog = new FakeCatalog();
-    const toothbrush: Product = new Product('toothbrush', ProductUnit.Each);
-    catalog.addProduct(toothbrush, 0.99);
-    const apples: Product = new Product('apples', ProductUnit.Kilo);
-    catalog.addProduct(apples, 1.99);
+const print = (receipt: Receipt): string => {
+  const receiptPrinter = new ReceiptPrinter();
 
-    const cart: ShoppingCart = new ShoppingCart();
-    cart.addItemQuantity(apples, 2.5);
+  return receiptPrinter.printReceipt(receipt);
+};
 
-    const teller: Teller = new Teller(catalog);
-    teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
-    teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, apples, 10.0);
+const printDiscount = (price: number, quantity: number, offerType: SpecialOfferType): string => {
+  const catalog: SupermarketCatalog = new FakeCatalog();
+  const apples: Product = new Product('apples', ProductUnit.Kilo);
+  catalog.addProduct(apples, price);
 
-    const receipt: Receipt = teller.checksOutArticlesFrom(cart);
+  const cart: ShoppingCart = new ShoppingCart();
+  cart.addItemQuantity(apples, quantity);
 
-    const receiptPrinter = new ReceiptPrinter();
+  const teller: Teller = new Teller(catalog);
+  teller.addSpecialOffer(offerType, apples, 10.0);
 
-    const printed = receiptPrinter.printReceipt(receipt);
+  const receipt: Receipt = teller.checksOutArticlesFrom(cart);
 
-    expect(printed).toBe(
-      `apples                              4.98
+  return print(receipt);
+};
+const APPLE_PRICE = 1.99;
+
+describe('Supermarket', () => {
+  describe('Multiple products', () => {
+    it('Should have multiple lines on ticket', () => {
+      const catalog: SupermarketCatalog = new FakeCatalog();
+      const apples: Product = new Product('apples', ProductUnit.Kilo);
+      catalog.addProduct(apples, APPLE_PRICE);
+
+      const cart: ShoppingCart = new ShoppingCart();
+      cart.addItemQuantity(apples, 1);
+      cart.addItemQuantity(apples, 1);
+
+      const teller: Teller = new Teller(catalog);
+
+      const receipt: Receipt = teller.checksOutArticlesFrom(cart);
+
+      expect(print(receipt)).toBe(`apples                              1.99
+apples                              1.99
+
+Total:                              3.98`);
+    });
+  });
+
+  describe('Two for amount', () => {
+    it('More than 2 products', () => {
+      const catalog: SupermarketCatalog = new FakeCatalog();
+      const apples: Product = new Product('apples', ProductUnit.Kilo);
+      catalog.addProduct(apples, APPLE_PRICE);
+
+      const cart: ShoppingCart = new ShoppingCart();
+      cart.addItemQuantity(apples, 1);
+      cart.addItemQuantity(apples, 1);
+
+      const teller: Teller = new Teller(catalog);
+      teller.addSpecialOffer(SpecialOfferType.TwoForAmount, apples, 10.0);
+
+      const receipt: Receipt = teller.checksOutArticlesFrom(cart);
+
+      expect(print(receipt)).toBe(`apples                              1.99
+apples                              1.99
+2 for 10(apples)                  --6.02
+
+Total:                             10.00`);
+    });
+
+    it('Less than 2 products', () => {
+      const catalog: SupermarketCatalog = new FakeCatalog();
+      const apples: Product = new Product('apples', ProductUnit.Kilo);
+      catalog.addProduct(apples, APPLE_PRICE);
+
+      const cart: ShoppingCart = new ShoppingCart();
+      cart.addItemQuantity(apples, 1);
+
+      const teller: Teller = new Teller(catalog);
+      teller.addSpecialOffer(SpecialOfferType.TwoForAmount, apples, 10.0);
+
+      const receipt: Receipt = teller.checksOutArticlesFrom(cart);
+
+      expect(print(receipt)).toBe(`apples                              1.99
+
+Total:                              1.99`);
+    });
+  });
+
+  describe('Discount for', () => {
+    it('ten percent amount', () => {
+      expect(printDiscount(APPLE_PRICE, 2.5, SpecialOfferType.TenPercentDiscount)).toBe(`apples                              4.98
   1.99 * 2.500
 10% off(apples)                    -0.50
 
-Total:                              4.48`
-    );
+Total:                              4.48`);
+    });
+    it('three for two', () => {
+      expect(printDiscount(APPLE_PRICE, 3, SpecialOfferType.ThreeForTwo)).toBe(`apples                              5.97
+  1.99 * 3.000
+3 for 2(apples)                    -1.99
+
+Total:                              3.98`);
+    });
+
+    it('five for amount', () => {
+      expect(printDiscount(APPLE_PRICE, 5, SpecialOfferType.FiveForAmount)).toBe(`apples                              9.95
+  1.99 * 5.000
+5 for 10(apples)                  --0.05
+
+Total:                             10.00`);
+    });
   });
 });
